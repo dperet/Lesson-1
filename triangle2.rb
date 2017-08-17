051b0c99fb7748f4b79e9c25970a927ae867780a
dmitry <peret201175@gmail.com>
2017-08-18 01:15:12 +0300

    Lesson 1

--- triangle2.rb
+++ triangle2.rb
@@ -7,11 +7,11 @@ side3 = gets.chomp.to_i
 if side1 == side2 || side2=side3 || side3 == side1
 	typeoftriangle = "isosceles"
 end
-if side1>side2 and side1>side3
+if side1 > side2 and side1 > side3
   hypotenuse = side1
   a = side2
   b = side3
-elsif side2>side1 and side2>side3
+elsif side2 > side1 and side2 > side3
   hypotenuse = side2
   a = side1
   b = side3
@@ -23,7 +23,7 @@ end
 if hypotenuse**2 == a**2 + b**2
   puts "Triangle is right-angled and #{typeoftriangle}"	
 end
-if side1==side2 and side1==side3
+if side1 == side2 and side1 == side3
   puts "Triangle is equilateral and #{typeoftriangle}"	
 else
 	puts "Triangle is not right-angled"
