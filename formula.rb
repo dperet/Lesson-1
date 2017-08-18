41285138f0b2e064958d2ba7cbd418a1531f28cf
dmitry <peret201175@gmail.com>
2017-08-18 14:00:34 +0300

    Lesson-1

--- formula.rb
+++ formula.rb
@@ -1,15 +1,21 @@
 puts "Please enter the factor a"
 a = gets.chomp.to_f
+
 puts "Please enter the factor b"
 b = gets.chomp.to_f
+
 puts "Please enter the factor c"
 c = gets.chomp.to_f
+
 d = b**2 - 4*a*c
+
 if d < 0
 	puts "D = #{d} . There is no radicals"
+
 elsif d == 0
 	x1 = - b + 0 / 2*a 
 	puts "D = #{d} . X1 = #{x1}"
+  
 else
 	k = Math.sqrt(d)
 	x1 = - b + k / 2*a
--- triangle2.rb
+++ triangle2.rb
@@ -1,12 +1,18 @@
 puts "Please enter 1st side of triangle"
 side1 = gets.chomp.to_i
+
 puts "Please enter 2nd side of triangle"
 side2 = gets.chomp.to_i
+
 puts "Please enter 3rd side of triangle"
 side3 = gets.chomp.to_i
-if side1 == side2 || side2=side3 || side3 == side1
+
+if side1 == side2 || side2 == side3 || side3 == side1
 	typeoftriangle = "isosceles"
+else
+  typeoftriangle = "not isosceles"
 end
+
 if side1 > side2 and side1 > side3
   hypotenuse = side1
   a = side2
@@ -20,11 +26,13 @@ else
   a = side1
   b = side2
 end
+
 if hypotenuse**2 == a**2 + b**2
   puts "Triangle is right-angled and #{typeoftriangle}"	
 end
+
 if side1 == side2 and side1 == side3
   puts "Triangle is equilateral and #{typeoftriangle}"	
 else
-	puts "Triangle is not right-angled"
+  puts "Triangle is not right-angled and #{typeoftriangle}"
 end
